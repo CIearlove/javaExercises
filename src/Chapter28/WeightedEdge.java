9672daabeca83a9a5040b5b74422a6fe7cada7c0
舵主 陈 <617316241@qq.com>
2019-03-18 17:57:59 +0800

    Chapter28

---
+++ src/Chapter28/WeightedEdge.java
@@ -0,0 +1,26 @@
+package Chapter28;
+
+import Chapter27.AbstractGraph;
+
+public class WeightedEdge extends AbstractGraph.Edge implements Comparable<WeightedEdge>{
+	
+	public int weight;//The weight on edge(u,v)
+	
+	public WeightedEdge(int u, int v,int weight) {
+		super(u, v);
+		this.weight = weight;
+	}
+/*
+ * Compare two edges on weights
+ */
+	public int compareTo(WeightedEdge edge) {
+		if(weight>edge.weight){
+			return 1;
+		}
+		else if(weight == edge.weight){
+			return 0;
+		}
+		else
+			return -1;
+	}
+}
